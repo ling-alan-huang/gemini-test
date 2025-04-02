23eca22e04886127b559b89c551cb79ca59eeced
Alan Huang <alan.huang@liferay.com>
2025-04-02 10:11:48 +0800

    add modues/apps/accessibility

---
+++ modues/apps/accessibility/accessibility-menu-web/build/node/packageRunBuild/resources/index.js
@@ -0,0 +1,10 @@
+
+import * as a9803fedbb1253ba9e0dc79dca1f20dd95e1eb93560963a3eb8e48ba8d3ed5ddc from "../../../../accessibility-menu-web/__liferay__/index.js";
+
+Liferay.Loader.define(
+	"@liferay/accessibility-menu-web@1.0.27/index",
+	['module'],
+	function (module) {
+		module.exports = a9803fedbb1253ba9e0dc79dca1f20dd95e1eb93560963a3eb8e48ba8d3ed5ddc;
+	}
+);
---
+++ modues/apps/accessibility/accessibility-settings-state-web/build/node/packageRunBuild/resources/index.js
@@ -0,0 +1,10 @@
+
+import * as a328561228f22496d1545d1bc065e3656120824b2eb239ad94fd45a3dd57022db from "../../../../accessibility-settings-state-web/__liferay__/index.js";
+
+Liferay.Loader.define(
+	"@liferay/accessibility-settings-state-web@1.0.13/index",
+	['module'],
+	function (module) {
+		module.exports = a328561228f22496d1545d1bc065e3656120824b2eb239ad94fd45a3dd57022db;
+	}
+);
